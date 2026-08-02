00bb4a887ed8918cf115ef42a51bf1b71af3fc34
zunaiddev <ja7667924@gmail.com>
2026-08-03 01:52:15 +0530

    Solved Optimal Approach

--- src/array/PairSum.java
+++ src/array/PairSum.java
@@ -5,7 +5,10 @@ import java.util.Arrays;
 public class PairSum {
     static void main(String[] args) {
         int[] arr = new int[]{2, 5, 11, 15};
-        System.out.println(Arrays.toString(bruteForce(arr, 17)));
+        final int target = 13;
+
+        System.out.println(Arrays.toString(bruteForce(arr, target)));
+        System.out.println(Arrays.toString(optimalApproach(arr, target)));
     }
 
     static int[] bruteForce(int[] arr, int target) {
@@ -22,11 +25,20 @@ public class PairSum {
 
     static int[] optimalApproach(int[] arr, int target) {
         int start = 0;
-        int end =
-        for (int start = 0; start < arr.length; start++) {
+        int end = arr.length - 1;
 
+        while (start < end) {
+            final int sum = arr[start] + arr[end];
+
+            if (sum < target) {
+                start++;
+            } else if (sum > target) {
+                end--;
+            } else {
+                return new int[]{start, end};
+            }
         }
 
-        return new int[]{};
+        return new int[]{start, end};
     }
 }
